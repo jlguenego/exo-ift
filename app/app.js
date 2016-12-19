7ce992242853010da96d889558a70d465e59e347
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-19 15:30:26 +0100

    footer

--- app/app.js
+++ app/app.js
@@ -8,5 +8,21 @@
 			templateUrl: './tmpl/ift-header.html'
 		};
 	});
+	
+	app.directive('iftFooter', function($http) {
+		return {
+			link: function(scope, element, attrs) {
+				console.log('link', arguments);
+				$http.get('./tmpl/ift-footer.html')
+					.then(function(response) {
+						var html = response.data;
+						element.html(html);
+					}).catch(function(error) {
+						console.error('error', error);
+					});
+				
+			}
+		};
+	});
 
 })();
