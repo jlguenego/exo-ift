d102d4ac6345af6b3233a61f59b1357a92de81d7
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-19 17:33:15 +0100

    gulp

--- server.js
+++ server.js
@@ -3,16 +3,27 @@ var serveIndex = require('serve-index');
 
 var app = express();
 
+// run with --prod if you want to test the prod stuff (you don't have the short dev loop)
+var prod = (process.argv[2] === '--prod');
+var htdocs = './app';
+var port = 8000;
+if (prod) {
+	htdocs = './dist';
+	port = 9000;
+}
 
-//webpack
-var webpack = require('webpack');
-var webpackConfig = require('./webpack.config.js');
-webpackConfig.output.path = '/';
-var compiler = webpack(webpackConfig);
-var webpackDevMiddleware = require('webpack-dev-middleware');
-app.use('/wpk/', webpackDevMiddleware(compiler, {
-	// options
-}));
+// for the short loop dev paradigm: express watches the files and build the bundles when needed.
+if (!prod) {
+	//webpack
+	var webpack = require('webpack');
+	var webpackConfig = require('./webpack.config.js');
+	webpackConfig.output.path = '/';
+	var compiler = webpack(webpackConfig);
+	var webpackDevMiddleware = require('webpack-dev-middleware');
+	app.use('/wpk/', webpackDevMiddleware(compiler, {
+		// options
+	}));
+}
 
 app.use(express.static('.'));
 app.use(serveIndex('.', {icons: true}));
@@ -23,6 +34,6 @@ app.use(function(req, res, next) {
 	next();
 });
 
-app.listen(8000, function() {
-	console.log('server started on port ' + 8000);
+app.listen(port, function() {
+	console.log('server started on port ' + port);
 });
