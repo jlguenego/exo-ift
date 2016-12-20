149650fb070adccd58939886f8fbad4eaf24fb6c
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-20 12:17:02 +0100

    watch ift-star

--- app/ift-star/ift-star.js
+++ app/ift-star/ift-star.js
@@ -5,36 +5,45 @@ var app = angular.module('ift-star', []);
 
 app.directive('iftStar', function() {
 	return {
+		scope: {
+			n: '=note'
+		},
 		link: function(scope, element, attrs) {
 			console.log('iftStar link', arguments);
-			var disabled = false;
-			var html = '';
-			var note = attrs.note;
-			if (note === undefined || note === '') {
-				disabled = true;
-			} else {
-				note = Number(note);
-				disabled = isNaN(note);
-			}
 			
-			
-			
-			if (disabled) {
-				for (var i = 0; i < 5; i++) {
-					html += '<img src="ift-star/img/gray_star.png" />';
+			scope.$watch('n', function() {
+				var disabled = false;
+				var html = '';
+				var note = scope.n;
+				if (note === undefined || note === '') {
+					disabled = true;
+				} else {
+					note = Number(note);
+					disabled = isNaN(note);
 				}
-			} else {
-				note = (note > 5) ? 5 : note;
-				note = (note < 0) ? 0 : note;
-				for (var i = 0; i < note; i++) {
-					html += '<img src="ift-star/img/yellow_star.png" />';
-				}
-				for (var i = note; i < 5; i++) {
-					html += '<img src="ift-star/img/white_star.png" />';
-				}
-			}
 				
-			element.html(html);
+				
+				
+				if (disabled) {
+					for (var i = 0; i < 5; i++) {
+						html += '<img src="ift-star/img/gray_star.png" />';
+					}
+				} else {
+					note = (note > 5) ? 5 : note;
+					note = (note < 0) ? 0 : note;
+					for (var i = 0; i < note; i++) {
+						html += '<img src="ift-star/img/yellow_star.png" />';
+					}
+					for (var i = note; i < 5; i++) {
+						html += '<img src="ift-star/img/white_star.png" />';
+					}
+				}
+					
+				element.html(html);
+				
+			});
+			
+			
 		}
 	};
 });
