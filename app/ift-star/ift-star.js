650c34bd1167ec6024085c58ee602577087264f6
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-20 11:32:52 +0100

    5 yellow star

--- app/ift-star/ift-star.js
+++ app/ift-star/ift-star.js
@@ -4,8 +4,13 @@ var app = angular.module('ift-star', []);
 
 app.directive('iftStar', function() {
 	return {
-		link: function() {
+		link: function(scope, element, attrs) {
 			console.log('iftStar link', arguments);
+			var html = '';
+			for (var i = 0; i < 5; i++) {
+				html += '<img src="ift-star/img/yellow_star.png" />';
+			}
+			element.html(html);
 		}
 	};
 });
