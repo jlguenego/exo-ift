07f835ada7809eab7ef2f4959e802cb5f3a8a6b2
Jean-Louis GUENEGO <jlguenego@gmail.com>
2016-12-20 14:14:55 +0100

    update

--- app/ift-star/ift-star.js
+++ app/ift-star/ift-star.js
@@ -3,13 +3,18 @@ require('./ift-star.css');
 
 var app = angular.module('ift-star', []);
 
-app.directive('iftStar', function() {
+app.directive('iftStar', function($compile) {
 	return {
 		scope: {
 			n: '=note'
 		},
 		link: function(scope, element, attrs) {
 			console.log('iftStar link', arguments);
+			
+			scope.update = function(note) {
+				console.log('update', arguments);
+				scope.n = note;
+			};
 			
 			scope.$watch('n', function() {
 				var disabled = false;
@@ -26,20 +31,23 @@ app.directive('iftStar', function() {
 				
 				if (disabled) {
 					for (var i = 0; i < 5; i++) {
-						html += '<img src="ift-star/img/gray_star.png" />';
+						html += '<img ng-click="update(' + (i+1) + ')" src="ift-star/img/gray_star.png" />';
 					}
 				} else {
 					note = (note > 5) ? 5 : note;
 					note = (note < 0) ? 0 : note;
 					for (var i = 0; i < note; i++) {
-						html += '<img src="ift-star/img/yellow_star.png" />';
+						html += '<img ng-click="update(' + (i+1) + ')" src="ift-star/img/yellow_star.png" />';
 					}
 					for (var i = note; i < 5; i++) {
-						html += '<img src="ift-star/img/white_star.png" />';
+						html += '<img ng-click="update(' + (i+1) + ')" src="ift-star/img/white_star.png" />';
 					}
 				}
 					
 				element.html(html);
+				var fn = $compile(element.contents());
+				//console.log('fn', fn.toString());
+				fn(scope);
 				
 			});
 			
